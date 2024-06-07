1687d013c34cee286c4885a0d604d6fa202cd4b7
HeitorSylvester <heitorrodrigues3105@gmail.com>
2024-06-07 08:52:50 -0400

    modified:   Paper 4/Exercise2.py

--- Paper 4/Exercise2.py
+++ Paper 4/Exercise2.py
@@ -1,7 +1,22 @@
-n=int(input())
-i=0
-list=[]
+n=int(input()) #6 dias
+i=0     
+list=[] 
+alpha=0
+beta=0
+vector=[]
 while n > i:
-    temps=int(input())
-    list.append(temps)
-    
+    temps=int(input()) #3,2,1=6
+    vector.append(temps) #VETOR SALVA MMAMAMAMAM
+    alpha=temps+alpha
+    beta+=temps
+    i+=1
+i-=1
+i=0
+alpha=[]
+beta=round(beta/n,2) #24.5
+while n > i:
+    vector[i]
+    if vector[i] > beta :
+        alpha.append(vector[i]) #<----- importante
+    i+=1
+print(beta,alpha)
