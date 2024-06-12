c24199eb1182170d2322eaf87e8db681ebde1933
HeitorSylvester <heitorrodrigues3105@gmail.com>
2024-06-12 08:50:03 -0400

    modified:   Paper 4/Exercise3.py 	modified:   Testing.py 	Paper 4/Exercise12.py 	Paper 4/Exercise3(better).py

--- Paper 4/Exercise3.py
+++ Paper 4/Exercise3.py
@@ -1,16 +1,22 @@
-answers=input("Digite o gabarito: ") #gabarito
+answers=input("Digite o gabarito: ").split() #gabarito
 qty_students=int(input()) #quantidade de alunos
 #questoes 6, de A até E
 i=0
 vector=[]
-answers.split()
-print (answers)
 while qty_students>i:
     student_answers=input()
     student_answers=student_answers.split()
-    i-=1
-i=0
+    vector.append(student_answers)
+    i+=1
 
-while qty_students>i:
-    print (vector[0],student_answers[0])
+
+i=0
+while i < qty_students:
+    j=0
+    acertos=0
+    while j < len(answers):
+        if answers[j] == vector[i][j]:
+            acertos+=1
+        j+=1
+    print (acertos)
     i+=1
