ac8ab764b85445f15d92dac055ad64b2db44eb02
HeitorSylvester <heitorrodrigues3105@gmail.com>
2024-06-22 17:15:59 -0400

    deleted:    Ativ_pratic.py 	deleted:    Miras.py

--- Ativ_pratic.py
+++
@@ -1,8 +0,0 @@
-i = 0
-vector = []
-while 100 > i:
-    values = float (input()) 
-    vector.append(values) 
-    if vector [i] <= 10:
-        print ("A" + "["+ str (i) + "] = " + "%0.1f" % (vector[i]) )
-    i+=1 
