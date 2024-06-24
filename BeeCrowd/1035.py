8b90efed4e0b2af8181fc2c67a2ac2dae54a1cdd
HeitorSylvester <heitorrodrigues3105@gmail.com>
2024-06-24 08:32:54 -0400

    new file:   BeeCrowd/1035.py

---
+++ BeeCrowd/1035.py
@@ -0,0 +1,11 @@
+value_a, value_b, value_c, value_d = input().split()
+value_a = int(value_a)
+value_b = int(value_b)
+value_c = int(value_c)
+value_d = int(value_d)
+sum_cd = value_c + value_d
+sum_ab = value_a + value_b
+if (value_b > value_c) and (value_d > value_a) and (sum_cd > sum_ab) and (value_a % 2 == 0) and (value_c +0) and (value_b +0):
+    print ("Valores aceitos")
+else:
+    print ("Valores nao aceitos")
