c9affedd73008b4e6a57a690c65069dceed01f07
HeitorSylvester <heitorrodrigues3105@gmail.com>
2024-06-24 23:24:22 -0400

    new file:   Paper 4/Exercise7.py 	modified:   Tests/TestBoard.py

---
+++ Paper 4/Exercise7.py
@@ -0,0 +1,20 @@
+times = int(input()) #qtd de valores pro vetor # 5
+vector = []
+i = 0 
+handy = 0
+j = 0  #5
+aux = times - 1
+while times > i: #aplicação dos valores no vetor
+    value = int(input())
+    vector.append(value)
+    i+=1
+# 5, 3, 2, 7, 9
+i = 0 
+while times > i: # verificação do menor valor 
+    if vector[i] == vector[aux]: #condição forte 
+    
+    aux -=1
+    i+=1
+print (handy)
+
+#UNFINISHED CODE
--- Tests/TestBoard.py
+++ Tests/TestBoard.py
@@ -1,3 +1,9 @@
-a = float(input())
-c = a / 3.1415
-print ("%0.3f"%c)
+# preciso fazer uma comparação a cada entrada de vetor
+# onde preciso armazenar o anterior 
+# e fazer uma comparação e verificar qual é o minimo
+# fiz a verificação porém como que a saida deve ser?
+#
+#5 
+# 5   3     7     2     9
+# vetor i ---> 
+# vetor aux <----
