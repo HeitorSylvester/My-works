3b165efbd255030505401af0b51684912160f8f7
HeitorSylvester <heitorrodrigues3105@gmail.com>
2024-06-25 12:19:36 -0400

    modified:   Paper 4/Exercise7.py

--- Paper 4/Exercise7.py
+++ Paper 4/Exercise7.py
@@ -2,6 +2,7 @@ times = int(input()) #qtd de valores pro vetor # 5
 vector = []
 i = 0 
 handy = 0
+handy_1 = 0
 j = 0  #5
 aux = times - 1
 while times > i: #aplicação dos valores no vetor
@@ -11,10 +12,10 @@ while times > i: #aplicação dos valores no vetor
 # 5, 3, 2, 7, 9
 i = 0 
 while times > i: # verificação do menor valor 
-    if vector[i] == vector[aux]: #condição forte 
-    
+    if vector [i-1] > vector [i]:
+        handy_1 = [i-1]
     aux -=1
     i+=1
-print (handy)
+print (handy_1)
 
 #UNFINISHED CODE
