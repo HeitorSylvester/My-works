a11a8ad9830f98c4e152ca27d8971b1115f84f95
HeitorSylvester <heitorrodrigues3105@gmail.com>
2024-06-25 13:07:47 -0400

    modified:   Paper 4/Exercise7.py

--- Paper 4/Exercise7.py
+++ Paper 4/Exercise7.py
@@ -1,8 +1,6 @@
 times = int(input()) #qtd de valores pro vetor # 5
 vector = []
 i = 0 
-handy = 0
-handy_1 = 0
 j = 0  #5
 aux = times - 1
 while times > i: #aplicação dos valores no vetor
@@ -11,11 +9,9 @@ while times > i: #aplicação dos valores no vetor
     i+=1
 # 5, 3, 2, 7, 9
 i = 0 
-while times > i: # verificação do menor valor 
-    if vector [i-1] > vector [i]:
-        handy_1 = [i-1]
-    aux -=1
+while times > i: # verificação do menor valor
+    if vector[j] > vector[i]:
+        aux = vector[i]
+        j+=1
     i+=1
-print (handy_1)
-
-#UNFINISHED CODE
+print (aux)
