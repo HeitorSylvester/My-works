dd78ba354255961c79a8cc28c5ae9c2c2623366f
HeitorSylvester <heitorrodrigues3105@gmail.com>
2024-07-01 12:53:10 -0400

    deleted:    Paper 1/Exercise 1.py

---
+++ Tests/Testing.py
@@ -0,0 +1,9 @@
+#REGISTER MENU
+print ("*************** CADASTRO ***************")
+student_name = input ("             DIGITE O NOME COMPLETO  \n").split()
+id = int(input ("         DIGITE O CPF DO ALUNO \n"))
+subs = input ("DIGITE A DICIPLINA PARA APLICAÇÃO (PARA MAIS DE UMA DICIPLINA ESCREVA COM ESPAÇOS) \n").split()
+print ("*******************************************")
+print (subs)
+
+
--- Tests/student_registration.py
+++ Tests/student_registration.py
@@ -1,4 +1,36 @@
-entrance = int (input("**** Olá qual será a ação hoje? ****\n")) #fazer um cadastro
+print ("********* Olá, qual será a ação hoje? *******")
+print ("  CADASTRO      --------- 1") #fazendo o cadastro
+print (" EDITAR ALUNO   --------- 2") 
+print (" REMOVER ALUNO  --------- 3")
+print ("LISTA DE ALUNOS --------- 4")
+print ("*******************************************")
+print ("ver. 0.0.1")
+
+
+entrance = int (input()) #fazer um cadastro
+#THE INTERFACE
+
+#THE MENU INPUTS
+register = 1
+edit_user = 2
+remove_user = 3
+student_lists = 4
 answer = True 
-while answer == True: #criação do menu
-        #VERY COMPLEX UNFINISHED
+#THE MENU ENTRANCE
+while answer == True:# AQUI ESTÃO AS ENTRADAS DO MENU PRINCIPAL
+    if register == 1:
+        #THE REGISTER MENU
+        print ("*************** CADASTRO ***************")
+        student_name = input ("             DIGITE O NOME COMPLETO  \n").split()
+        id = int(input ("         DIGITE O CPF DO ALUNO \n"))
+        subs = input ("DIGITE A DICIPLINA PARA APLICAÇÃO (PARA MAIS DE UMA DICIPLINA ESCREVA COM ESPAÇOS) \n").split()
+        print ("*******************************************")
+        print (subs)
+    elif edit_user == 2:
+        answer = False
+    elif remove_user == 3:
+        answer = False
+    elif student_lists == 4:
+        answer == False
+
+#STILL ONGOING PROJECT 
